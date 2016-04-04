4dbbb5bcbd1ccd13f314adb35c8fb23e0152c4ff
Riverbell <emba93@live.se>
2016-04-04 15:58:43 +0200

    Testing controller

--- app/js/testCtrl.js
+++ app/js/testCtrl.js
@@ -5,4 +5,23 @@ quizApp.controller('TestCtrl', function ($scope, Quiz) {
 	};
 
 	$scope.test();
+
+	$scope.getPlaylist = function() {
+		// gets the playlist
+		//console.log("YO");
+		$scope.tracks = Quiz.getPlaylist('https://open.spotify.com/user/113325595/playlist/5U6ibJ4AW3keswEmMhhtNP', function(data){
+			//console.log("hey");
+			//console.log("YOYO",data.tracks.items);
+			$scope.createQuestions(data.tracks.items);
+		});
+		//console.log("TRACKS",$scope.tracks);
+	}
+
+
+	$scope.getPlaylist();
+
+	$scope.createQuestions = function(tracks) {
+		console.log("This is my tracks",tracks);
+		Quiz.createQuestions(tracks);
+	}
 });
