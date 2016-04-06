b2a7ee590c5f4263fe3d0b6b82a7d2eaa712eb36
Riverbell <emba93@live.se>
2016-04-06 10:58:41 +0200

    Start game button now works, it creates questions and redirects to /game Also, moved d3 to new folder 'library'

--- app/js/gameCtrl.js
+++ app/js/gameCtrl.js
@@ -4,10 +4,27 @@ quizApp.controller('GameCtrl', function ($scope, Quiz) {
 		console.log('test');
 	};
 
-	$scope.test();
+	//$scope.test();
+	$scope.playlistInput = null;
 
-	$scope.getPlaylist = function() {
+	$scope.getPlaylist = function(link) {
 		// gets the playlist
-		
+		// bad playlist https://open.spotify.com/user/113325595/playlist/5U6ibJ4AW3keswEmMhhtNP
+		// good playlist 'https://open.spotify.com/user/hhawthorn/playlist/1Hue9EGWG1xl9UPcgji9vX'
+		console.log('This is link',link);
+		$scope.tracks = Quiz.getPlaylist(link, function(data){
+		//$scope.tracks = Quiz.getPlaylist('https://open.spotify.com/user/113325595/playlist/5U6ibJ4AW3keswEmMhhtNP', function(data){
+			//console.log("hey");
+			//console.log("YOYO",data.tracks.items);
+			$scope.createQuestions(data.tracks.items);
+		});
+		//console.log("TRACKS",$scope.tracks);
+	}
+
+
+	$scope.createQuestions = function(tracks) {
+		//console.log("This is my tracks",tracks);
+		$scope.questions = Quiz.createQuestions(tracks);
+		console.log("These are our questions!",$scope.questions);
 	}
 });
