30030516980e98fbf73291bafdd0260b18fb4b7c
Maria Westling <kemwestling@gmail.com>
2016-04-09 14:17:37 +0200

    if else to determine action for chosen answer, change of color, reset of color between questions

--- app/js/gameCtrl.js
+++ app/js/gameCtrl.js
@@ -30,18 +30,44 @@ quizApp.controller('GameCtrl', function ($scope, $routeParams, Quiz) {
 			//hide button
 			//butt.html('');
 		}
-
+		
+		$scope.updateDivColor();
 	}
 
-	$scope.validateAnswer = function(divObj) {
-		console.log("YES");
+	$scope.updateDivColor = function() {
+		var id = ["alt1", "alt2", "alt3", "alt4"];
+		for (i in id) {
+			div = document.getElementById(id[i]);
+			div.style.background="#FCE4EC";
+		}
+	}
+
+	$scope.validateAnswer = function(alt, alternative) {
+
+		var altID = "alt" + alt;
+		var divID = document.getElementById(altID);
+		console.log("question", $scope.question().correctAnswer);
+
+		if (alternative !== $scope.question().correctAnswer) {
+			console.log("YES")
+			//fel svar blir rött när en klickar på det
+			divID.style.background="#EF5350";
+		}
+		else {
+			//rätt svar blir grönt när en klickar på det
+			divID.style.background="#26A69A"
+		}
 		
 		//göm forward
-		//rätt svaret ska lysa grönt direkt
+		//rätt svar ska lysa grönt direkt (?)
 		//om rätta svaret är klickat, do nothing, 
 		//om fel svar sätt div till röd färg
 		//sätt divar till unclickable
-		//divObj.style.background="#EF5350";
+		
+	}
+
+	$scope.correctAnswer = function() {
+
 	}
 
 
